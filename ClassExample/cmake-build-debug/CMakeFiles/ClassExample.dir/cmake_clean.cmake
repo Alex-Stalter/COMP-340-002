7c81c825d16ba58edae03a8fa047bbafc9bc8a94
Unknown <scooterblenny417@gmail.com>
2019-04-29 12:55:57 -0400

    Added main

---
+++ ClassExample/cmake-build-debug/CMakeFiles/ClassExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassExample.dir/main.cpp.o"
+  "ClassExample.pdb"
+  "ClassExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
