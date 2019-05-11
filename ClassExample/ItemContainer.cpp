5d666618a8b56fdffcc2b8b4581d94a4b1789199
Alex Stalter <scooterblenny417@gmail.com>
2019-05-11 17:35:53 -0400

    Added Commands and Item Class to be able to loot sectors

---
+++ ClassExample/ItemContainer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex Stalter on 2019-05-11.
+//
+
+#include "ItemContainer.h"
---
+++ ClassExample/ItemContainer.h
@@ -0,0 +1,14 @@
+//
+// Created by Alex Stalter on 2019-05-11.
+//
+
+#ifndef CLASSEXAMPLE_ITEMCONTAINER_H
+#define CLASSEXAMPLE_ITEMCONTAINER_H
+
+
+class ItemContainer {
+
+};
+
+
+#endif //CLASSEXAMPLE_ITEMCONTAINER_H
