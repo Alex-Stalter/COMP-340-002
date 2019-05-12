1c4eeff35ab25bfac4296de84a7c8a581d462f9c
Raina <rlmalmberg53@gmail.com>
2019-05-11 22:19:19 -0400

    Added a base for Bug Ships
    Scorpion race, needs randomization for ship model

--- ClassExample/Game.cpp
+++ ClassExample/Game.cpp
@@ -53,7 +53,7 @@ void Game::combat() {
 void Game::instatiateGame() {
     std::cout<<"Your ship arrives in the system you only have 12 jumps to ready yourself before you have to face the rebel leader.\n"
                "If you destroy enough enemy ships you'll be able to access the blockaded sectors of this system.\n"
-               "Good luc captain!"<<std::endl;
+               "Good luck, captain!"<<std::endl;
     this->currentRoom = this->newMap->getList()[0];
     std::cout<<"You are in "<< this->currentRoom->getDescription() << std::endl;
     while(this->userInput != "Give Up"){
@@ -62,6 +62,10 @@ void Game::instatiateGame() {
         }
         std::cout<<"What would you like to do or where would you like to go? You can always just 'Give Up'. To see a list of commands type "
                    "'Get Help'"<<std::endl;
+        std::cout<<"What would you like to do or where would you like to go? You can always just 'Give Up'."<<std::endl;
+        //std::getline(std::cin, this->userInput);
+        std::cin>>userInput;
+         if(this->userInput=="Search"){
 
         std::cin>>userInput>>secondInput;
          if(this->userInput=="Search"){
@@ -80,10 +84,10 @@ void Game::instatiateGame() {
         }else if(this->userInput == "Open"){
             if(this->secondInput=="Blockade"&&this->newMap->getList()[10]->isLocked()&&this->enemiesKilled>=2){
                 this->newMap->getList()[10]->setLock(false);
-                std::cout<<"Captain the blockade around sector 11 has been lifted and we are now free to travel there"<<std::endl;
+                std::cout<<"Captain, the blockade around sector 11 has been lifted and we are now free to travel there"<<std::endl;
             }else if(this->secondInput=="Blockade"&&this->newMap->getList()[1]->isLocked()&&this->enemiesKilled>=4){
                 this->newMap->getList()[1]->setLock(false);
-                std::cout<<"Captain the blockade around sector 1 has been lifted and we are free to travel there."<< std::endl;
+                std::cout<<"Captain, the blockade around sector 1 has been lifted and we are free to travel there."<< std::endl;
             }
 
         }else if(this->userInput == "Look"){
@@ -100,6 +104,10 @@ void Game::instatiateGame() {
              }
 
         }else if(this->userInput=="Give" && this->secondInput=="Up"){
+
+
+
+        }else if(this->userInput=="GiveUp"){
             break;
         }else if(this->userInput=="Use"){
              int count=-1;
